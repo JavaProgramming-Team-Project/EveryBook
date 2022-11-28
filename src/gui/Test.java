e99f151a46f1adf6da0b21d045d4214bc9006bec
JayPark99 <115969860+JayPark99@users.noreply.github.com>
2022-11-29 05:11:47 +0900

    Update Test.java

--- src/gui/Test.java
+++ src/gui/Test.java
@@ -3,7 +3,7 @@ package gui;
 import login.LoginMember;
 
 public class Test {
-
+//test
 	public static void main(String[] args) {
 		LoginMember.setLoginMember(null);
 		MainFrame mf = new MainFrame();
