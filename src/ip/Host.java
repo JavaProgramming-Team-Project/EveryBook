8456f889121d7270b5c22628ae8140da6b83d747
정주원 <filocd05155@naver.com>
2022-12-01 16:08:32 +0900

    Update Host.java

--- src/ip/Host.java
+++ src/ip/Host.java
@@ -1,7 +1,7 @@
 package ip;
 
 public class Host {
-    private static String host = "http://180.69.79.224:9999";
+    private static String host = "http://34.64.71.14:9999";
     // http://localhost:8080
 
     public static String getHost() {
